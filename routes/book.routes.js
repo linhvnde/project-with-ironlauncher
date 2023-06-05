116f63482eb895dfc9555baa2e2b6366ae4aa962
linhvnde <linh.nh.tyty@gmail.com>
2023-06-05 16:37:05 +0200

    WIP for edit and delete

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -23,12 +23,54 @@ router.get("/books", (req, res, next) => {
     });
 });
 
+/* GET /books/create CREATE FORM*/
+router.get("/books/create", (req, res, next) => {
+  res.render("books/book-create");
+});
+/* POST /books/create PROCESS FORM */
+router.post("/books/create", (req, res, next) => {
+  console.log(req.body);
+  const newBook = {
+    //the data here need to match with the model
+    //the properties here match with the input Name in the form
+    title: req.body.title,
+    description: req.body.description,
+    author: req.body.author,
+    rating: req.body.rating,
+  };
+
+  Book.create(newBook)
+    .then((newBook) => {
+      console.log(newBook);
+      res.redirect("/books");
+    })
+    .catch((e) => {
+      console.log("error creating new book", e);
+      next(e);
+    });
+});
+
+// GET route to display the form to update a specific book
+router.get("/books/:bookId/edit", (req, res, next) => {
+  const { bookId } = req.params;
+
+  Book.findById(bookId)
+    .then((bookToEdit) => {
+      console.log(bookToEdit);
+    })
+    .catch((e) => {
+      console.log("error creating new book", e);
+      next(e);
+    });
+});
+
 /* GET /books/:bookId */
 router.get("/books/:bookId", (req, res, next) => {
   const bookId = req.params.bookId;
   // console.log(bookId);
   Book.findById(bookId)
-    .then((oneBookFromDB) => {//oneBookFromDB is already an object so  in the line 38 dont need the {}
+    .then((oneBookFromDB) => {
+      //oneBookFromDB is already an object so  in the line 38 dont need the {}
       // res.send(`THIS IS THE ONE BOOK FROM DB:${{ oneBookFromDB }}`);
 
       // console.log("THIS IS THE BOOK", oneBookFromDB);
