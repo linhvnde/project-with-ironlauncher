32d17773aefb3ebafa478e6fdbf27f7dee064076
linhvnde <linh.nh.tyty@gmail.com>
2023-06-07 14:19:50 +0200

    form validations with error messages

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const router = express.Router();
 const bcryptjs = require("bcryptjs");
-const saltRounds = 10;
+const { default: mongoose } = require("mongoose");
+
+const router = express.Router();
+
 const User = require("../models/User.model");
+
+const saltRounds = 10;
 
 // ROUTES
 
@@ -14,6 +18,25 @@ router.get("/signup", (req, res, next) => {
 /////POST /signup
 router.post("/signup", (req, res, next) => {
   const { email, password } = req.body;
+  //////// make sure users fill all mandatory fields:
+  if (!email || !password) {
+    res.render("auth/signup", {
+      errorMessage:
+        "All fields are mandatory. Please provide your email and password.",
+    });
+    return; // finish execution of the current function
+  }
+
+  // make sure passwords are strong:
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res.status(400).render("auth/signup", {
+      errorMessage:
+        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
+    });
+    return;
+  }
+
   ////////before create an account need to encript the password
   bcryptjs
     .genSalt(saltRounds) // this method returns promise
@@ -33,7 +56,28 @@ router.post("/signup", (req, res, next) => {
     })
     .catch((error) => {
       console.log("error creating account...", error);
-      next(error);
+      if (error instanceof mongoose.Error.ValidationError) {
+        console.log("this is a mongoose validator error");
+        res.status(400).render("auth/signup", {
+          errorMessage: error.message,
+        }); //this error message will be in the mongoose model });
+      } else if (error.code === 11000) {
+        res
+          .status(400)
+          .render("auth/signup", { errorMessage: "Email needs to be unique." });
+      } else {
+        console.log("this is NOT a mongoose validator error");
+        next(error);
+      }
+      /* if (error instanceof mongoose.Error.ValidationError) {
+                console.log("this is a mongoose validator error")
+                res.status(400).render('auth/signup', { errorMessage: error.message });
+            } else if (error.code === 11000) {
+                res.status(400).render('auth/signup', { errorMessage: 'Email needs to be unique.' });
+            } else {
+                console.log("this is NOT a mongoose validator error")
+                next(error)
+            } */
     });
 });
 
